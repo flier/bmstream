fe05f581f01484b94fc4e4e9c2a2f98b6696ffc1
Flier Lu <flier.lu@gmail.com>
2015-06-01 22:01:36 +0800

    use strict DI mode

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -6,11 +6,11 @@ module bookmarks {
   var app = angular.module('app', [
     'ngAnimate', 'ngAria', 'ngCookies', 'ngMessages', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'ngMaterial',
     'app.services', 'app.directives', 'app.controllers.ui',  'app.controllers.bookmark'
-  ]).config(function ($routeProvider:angular.route.IRouteProvider) {
+  ]).config(['$routeProvider', function ($routeProvider:angular.route.IRouteProvider) {
       $routeProvider
         .when('/tiles', {templateUrl: 'views/tiles.html', controller: 'TilesCtrl'})
         .when('/timeline', {templateUrl: 'views/timeline.html', controller: 'TimelineCtrl'})
         .when('/tagcloud', {templateUrl: 'views/tagcloud.html', controller: 'TagCloudCtrl'})
         .otherwise({redirectTo: '/tiles'});
-    });
+    }]);
 }
